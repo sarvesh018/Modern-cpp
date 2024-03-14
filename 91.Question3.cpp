d1ab6e55d46d23ac0f08e779c249bd7e305e95ad
Sarvesh <sarveshc111@gmail.com>
2024-03-14 17:20:55 +0530

    perfect forwarding

--- 91.Question3.cpp
+++ 91.Question3.cpp
@@ -24,21 +24,46 @@ class Array{
         }
 
         void setValue(T&& value, T&& index){
+            std::cout<<"Inside rvalue"<<std::endl;
             array[index] = value;
         }
 
         void setValue(T& value, T& index){
+            std::cout<<"Inside lvalue"<<std::endl;
             array[index] = value;
         }
 
         template <typename F>
-        void setArrayValue(F value, F index){                           // perfect forwarding
+        void setArrayValue(F&& value, F&& index){                           // perfect forwarding
             setValue(std::forward<F>(value), std::forward<F>(index));   // calls lValue or rValue function
+        }
+
+        int operator[](int ind){
+            return array[ind];
         }
 
         ~Array() = default;                   // default destructor
         
 };
+
+template<typename T>
+void setValue(T&& value, T&& index){
+    std::cout<<"Inside rvalue"<<std::endl;
+    // array[index] = value;
+}
+
+template<typename T>
+void setValue(T& value, T& index){
+    std::cout<<"Inside lvalue"<<std::endl;
+    // array[index] = value;
+}
+
+template <typename F>
+void setArrayValue(F&& value, F&& index){                           // perfect forwarding
+    setValue(std::forward<F>(value), std::forward<F>(index));   // calls lValue or rValue function
+}
+
+
 
 int main(){
     Array<int64_t> array1;
@@ -52,5 +77,11 @@ int main(){
     int ind = 0;
     array2.setArrayValue(val, ind);         // passing lValue  
     array2.setArrayValue(20, 1);            // passing as rValue
+
+    setArrayValue(val, ind);         // passing lValue      // perfect forwarding works fine outside class
+    setArrayValue(20, 1);            // passing as rValue
+
+    std::cout<<array1[ind]<<" "<<array1[1]<<std::endl;
+    std::cout<<array2[ind]<<" "<<array2[1]<<std::endl;
 }
 
--- temp.cpp
+++ temp.cpp
@@ -1,49 +1,84 @@
 #include <iostream>
-#include <functional>
-#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <memory>
 
-using S = std::string;
+// Forward declaration of Observer interface
+class Observer;
 
-S convertToCaps(std::string str){
-    for(int i=0; i<str.size(); i++){
-        str[i] = str[i]-32;
+// Subject interface
+class Subject {
+public:
+    virtual ~Subject() {}
+    virtual void attach(std::shared_ptr<Observer> observer) = 0;
+    virtual void detach(std::shared_ptr<Observer> observer) = 0;
+    virtual void notify() = 0;
+};
+
+// Observer interface
+class Observer {
+public:
+    virtual ~Observer() {}
+    virtual void update(const std::string& data) = 0;
+};
+
+// Concrete subject representing a sensor
+class Sensor : public Subject {
+private:
+    std::vector<std::shared_ptr<Observer>> observers;
+    std::string data;
+public:
+    void attach(std::shared_ptr<Observer> observer) override {
+        observers.push_back(observer);
     }
-    return str;
-}
 
-S addCurlyBracket(std::string str){
-    return "{ " + str + " }";
-}
+    void detach(std::shared_ptr<Observer> observer) override {
+        auto it = std::find_if(observers.begin(), observers.end(), [&](const std::shared_ptr<Observer>& ptr) {
+            return ptr.get() == observer.get();
+        });
+        if (it != observers.end()) {
+            observers.erase(it);
+        }
+    }
 
-S addAdot(std::string str){
-    return str+"..";
-}
 
-S addHello(std::string str){
-    return "HELLO " + str;
-}
+    void notify() override {
+        for (const auto& observer : observers) {
+            observer->update(data);
+        }
+    }
 
-S acceptAString(std::string str){
-    return str;
-}
+    void setData(const std::string& newData) {
+        data = newData;
+        notify();
+    }
+};
 
-template<typename Func, typename Rest>
-auto compose(Func func, Rest rest){
-    return [func, rest](const auto& args){
-        return func(rest(args));
-    };
-}
+// Concrete observer representing a monitoring component
+class MonitoringComponent : public Observer {
+public:
+    void update(const std::string& data) override {
+        std::cout << "MonitoringComponent received update: " << data << std::endl;
+    }
+};
 
-template<typename Func, typename... Rest>
-auto compose(Func func, Rest... rest){
-    return [func, rest...](const auto& args){
-        return func(compose(rest...)(args));
-    };
-}
+int main() {
+    // Create sensor and monitoring component
+    std::shared_ptr<Sensor> sensor = std::make_shared<Sensor>();
+    std::shared_ptr<MonitoringComponent> monitoringComponent = std::make_shared<MonitoringComponent>();
 
-int main(){
-    auto functionComposition = compose(addCurlyBracket, addHello, addAdot, convertToCaps, acceptAString);
-    auto result = functionComposition("sarvesh");
+    // Attach monitoring component to sensor
+    sensor->attach(monitoringComponent);
 
-    std::cout<<result;
+    // Simulate data updates from sensor
+    sensor->setData("Temperature: 25C");
+    sensor->setData("Motion detected");
+
+    // Detach monitoring component from sensor
+    sensor->detach(monitoringComponent);
+
+    // Simulate another data update (should not be received by monitoring component)
+    sensor->setData("Window opened");
+
+    return 0;
 }
